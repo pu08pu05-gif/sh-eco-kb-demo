631f73d2091a9d9de0afbb2b79cf3eeb3974844d
pu08pu05-gif <pu08pu05@gmail.com>
2026-09-21 15:22:41 +0800

    update data/tech.js

---
+++ data/tech.js
@@ -0,0 +1,483 @@
+window.DATA = window.DATA || {};
+window.DATA.tech = [
+{
+"name": "城镇污泥厌氧消化处理优化提升技术",
+"type": "城镇污泥安全处置及资源化",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592163330662469.shtml",
+"date": "",
+"tags": [
+"水环境",
+"固体废物"
+],
+"id": "tech-001"
+},
+{
+"name": "城市河道水循环净化综合调控技术",
+"type": "城镇污水提质增效与管网优化",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592162317381701.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-002"
+},
+{
+"name": "城镇污水高效节地生物膜处理技术",
+"type": "城镇污水提质增效与管网优化",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592161336684613.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-003"
+},
+{
+"name": "城市污水深度脱氮除磷与再生回用技术",
+"type": "城镇污水提质增效与管网优化",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592159712825413.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-004"
+},
+{
+"name": "大型钢铁园区水污染全过程控制技术",
+"type": "工业企业及园区污染防治",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592158868398149.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-005"
+},
+{
+"name": "石化废水回用冷却水处理技术",
+"type": "工业企业及园区污染防治",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/591967238230085.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-006"
+},
+{
+"name": "农村生活污水处理设施智慧监控运行管理技术",
+"type": "农业农村面源污染控制",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/591922982809669.shtml",
+"date": "",
+"tags": [
+"水环境",
+"环境监测与信息化"
+],
+"id": "tech-007"
+},
+{
+"name": "畜禽养殖粪污资源化利用技术",
+"type": "农业农村面源污染防治",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/591918700068933.shtml",
+"date": "",
+"tags": [
+"固体废物"
+],
+"id": "tech-008"
+},
+{
+"name": "渗滤液收集回收利用技术该技术利用输送管道将回水池渗滤液用泵提压后返回厂内回用，作为选矿用水和冲渣用水",
+"type": "",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/591920964235333.shtml",
+"date": "",
+"tags": [
+"水环境",
+"固体废物"
+],
+"id": "tech-009"
+},
+{
+"name": "爱能森赫爱斯（HSES）近零碳供冷热技术一、基本原理爱能森赫爱斯(HSES)近零碳供冷热技术是以储能",
+"type": "",
+"field": "",
+"scene": "",
+"issue": "",
+"practice": "爱能森赫爱斯(HSES)近零碳供冷热技术是以储能为自主创新，以“储能+多能互补+智慧能",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/580145160462405.shtml",
+"date": "",
+"tags": [
+"气候变化与双碳",
+"环境监测与信息化"
+],
+"id": "tech-010"
+},
+{
+"name": "城镇污泥厌氧消化处理优化提升技术\u0001",
+"type": "城镇污泥安全处置及资源化\u0001",
+"field": "技术展厅",
+"scene": "",
+"issue": "污",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592163330662469.shtml",
+"date": "",
+"tags": [
+"水环境",
+"固体废物"
+],
+"id": "tech-011"
+},
+{
+"name": "城市河道水循环净化综合调控技术\u0001",
+"type": "城镇污水提质增效与管网优化\u0001",
+"field": "技术展厅",
+"scene": "",
+"issue": "河",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592162317381701.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-012"
+},
+{
+"name": "城镇污水高效节地生物膜处理技术\u0001",
+"type": "城镇污水提质增效与管网优化\u0001",
+"field": "技术展厅",
+"scene": "",
+"issue": "污",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592161336684613.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-013"
+},
+{
+"name": "城市污水深度脱氮除磷与再生回用技术\u0001",
+"type": "城镇污水提质增效与管网优化\u0001解决的难点问",
+"field": "技术展厅",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592159712825413.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-014"
+},
+{
+"name": "大型钢铁园区水污染全过程控制技术\u0001",
+"type": "工业企业及园区污染防治\u0001",
+"field": "技术展厅",
+"scene": "",
+"issue": "全流",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/592158868398149.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-015"
+},
+{
+"name": "石化废水回用冷却水处理技术\u0001",
+"type": "工业企业及园区污染防治\u0001",
+"field": "技术展厅",
+"scene": "",
+"issue": "工业冷却水",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/591967238230085.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-016"
+},
+{
+"name": "农村生活污水处理设施智慧监控运行管理技术\u0001",
+"type": "农业农村面源污染控制\u0001解决的难点问",
+"field": "技术展厅",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/591922982809669.shtml",
+"date": "",
+"tags": [
+"水环境",
+"环境监测与信息化"
+],
+"id": "tech-017"
+},
+{
+"name": "畜禽养殖粪污资源化利用技术\u0001",
+"type": "农业农村面源污染防治\u0001",
+"field": "技术展厅",
+"scene": "",
+"issue": "畜禽养殖过程",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台 · 技术展厅",
+"url": "https://www.ceett.org.cn/kjcg/jszt/591918700068933.shtml",
+"date": "",
+"tags": [
+"固体废物"
+],
+"id": "tech-018"
+},
+{
+"name": "一种锂电池电解液回收方法",
+"type": "",
+"field": "国家技术成果库",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台",
+"url": "https://www.ceett.org.cn/kjcg/jscg/839534438551621.shtml",
+"date": "",
+"tags": [],
+"id": "tech-019"
+},
+{
+"name": "煤基固废人工造土造地技术",
+"type": "",
+"field": "国家技术成果库",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台",
+"url": "https://www.ceett.org.cn/kjcg/jscg/839533371490373.shtml",
+"date": "",
+"tags": [
+"固体废物"
+],
+"id": "tech-020"
+},
+{
+"name": "一体化污泥连续碳化技术",
+"type": "",
+"field": "国家技术成果库",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台",
+"url": "https://www.ceett.org.cn/kjcg/jscg/834303370907717.shtml",
+"date": "",
+"tags": [
+"水环境",
+"气候变化与双碳"
+],
+"id": "tech-021"
+},
+{
+"name": "基于AI技术的浮游生物自动监测装备",
+"type": "",
+"field": "国家技术成果库",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台",
+"url": "https://www.ceett.org.cn/kjcg/jscg/834232035524677.shtml",
+"date": "",
+"tags": [
+"环境监测与信息化"
+],
+"id": "tech-022"
+},
+{
+"name": "多元生物联合预警技术及装备",
+"type": "",
+"field": "国家技术成果库",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台",
+"url": "https://www.ceett.org.cn/kjcg/jscg/834231966445637.shtml",
+"date": "",
+"tags": [
+"环境监测与信息化"
+],
+"id": "tech-023"
+},
+{
+"name": "一种含氟废水用液体除氟剂及其制备方法和应用",
+"type": "",
+"field": "国家技术成果库",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台",
+"url": "https://www.ceett.org.cn/kjcg/jscg/824682098204741.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-024"
+},
+{
+"name": "高盐废水生化系统增效技术（ME菌剂）",
+"type": "",
+"field": "国家技术成果库",
+"scene": "",
+"issue": "",
+"practice": "",
+"effect": "",
+"owner": "",
+"stage": "",
+"desc": "",
+"source": "国家生态环境科技成果转化综合服务平台",
+"url": "https://www.ceett.org.cn/kjcg/jscg/824682084388933.shtml",
+"date": "",
+"tags": [
+"水环境"
+],
+"id": "tech-025"
+}
+];
